bd2fbb8edcf9ea3c60ba3ac85e4dbd835a7f12ea
Sophie Kolbe <soko@Sophies-Air.fritz.box>
2020-06-26 13:11:32 +0200

    add pocket controls to cancel or conduct transaction and show error message when impossible

--- src/component/converter.js
+++ src/component/converter.js
@@ -11,6 +11,7 @@ class Converter extends React.Component {
             targetCurrency: "EUR",
             amount: 0,
             rate: null,
+            invert: null,
             currencies: ["EUR", "GBP", "USD"],
             pockets: [
                 {
@@ -33,9 +34,12 @@ class Converter extends React.Component {
     }
     componentDidMount() {
         this.getRate()
+        this.interval = setInterval(() =>  this.getRate(), 10000);
+    };
+    componentWillUnmount() {
+        clearInterval(this.interval);
     };
     getRate = () => {
-        //TODO: call this in fixed interval and store data in state
         //TODO: add/subtract amounts from pockets
         //TODO: change pockets ?
         //TODO: add tests
@@ -50,7 +54,9 @@ class Converter extends React.Component {
             )
             .then(response => {
                 const rate = response.data.rates[this.state.targetCurrency];
+                const invert = 1 / rate 
                 this.setState({ rate: rate.toFixed(2) });
+                this.setState({ invert: invert.toFixed(2) });
             })
             .catch(error => {
                 console.log("Error Occured when Fetching Rates", error.message);
@@ -61,6 +67,20 @@ class Converter extends React.Component {
         const change = event.target.value * this.state.rate;
         this.setState({ change: change.toFixed(2) });
     };
+    clearInputs = () => {
+        this.setState({ change: 0 })
+        this.setState({ amount: 0 })
+    };  
+    updatePocket = (source,target) => {
+        if(this.state.amount > source.amount) return
+        const pockets = this.state.pockets
+        pockets.forEach( _ => {
+            if(_.currency === target.currency) _.amount = (_.amount + parseFloat(this.state.change)).toFixed(2)
+            if(_.currency === source.currency) _.amount = (_.amount - parseFloat(this.state.amount)).toFixed(2)
+        }) 
+        this.setState({ pockets: pockets })
+        this.clearInputs()
+    };
     render() {
         const source = this.state.pockets.find(_ => _.currency === this.state.sourceCurrency);
         const target = this.state.pockets.find(_ => _.currency === this.state.targetCurrency);
@@ -69,15 +89,19 @@ class Converter extends React.Component {
                 <h1>
                     Convert Currencies
                 </h1>
-                <span>
-                    1 {source.symbol} = {this.state.rate} {target.symbol}
-                </span>
+                <div className="controls">
+                    <button onClick={_ => this.clearInputs()} >Cancel</button>    
+                    <span>
+                     {source.symbol} 1 = {target.symbol} {this.state.rate} 
+                    </span>
+                    <button onClick={_ =>this.updatePocket(source,target)}>Exchange</button>  
+                </div>
                 <div className="source">
                     <div className="pocket">
-                        <label>
+                        <label className="currency">
                             {source.currency}
                         </label>
-                        <span>
+                        <span className="amount">
                             You have {source.symbol} {source.amount}
                         </span>
                     </div>
@@ -90,21 +114,27 @@ class Converter extends React.Component {
                         onChange={event => this.convertCurrency(event)}
                     />
                 </div>
+                {this.state.amount > source.amount ? <h5>There is not enough credit available for this transaction.</h5>  : ''} 
                 <div className="target">
                     <div className="pocket">
-                        <label>
+                        <label className="currency">
                             {target.currency}
                         </label>
-                        <span>
+                        <span className="amount">
                             You have {target.symbol} {target.amount}
                         </span>
                     </div>
-                    <input
-                        name="amount"
-                        type="number"
-                        readOnly
-                        value={this.state.change}
-                    />
+                    <div className="exchange">
+                        <input
+                            name="amount"
+                            type="number"
+                            readOnly
+                            value={this.state.change}
+                        />
+                        <span className="note">
+                            {target.symbol} 1 = {source.symbol} {this.state.invert} 
+                        </span>
+                    </div>
                 </div>
             </div>
         );
