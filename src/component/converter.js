e248ea6fff4caf09ac718bc2b8d50f9845c83be9
Sophie Kolbe <soko@Sophies-Air.fritz.box>
2020-06-26 15:00:15 +0200

    adjust layout, styling and handling of empty input plus insufficient balance

--- src/component/converter.js
+++ src/component/converter.js
@@ -6,45 +6,43 @@ class Converter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            change: 0,
             sourceCurrency: "GBP",
             targetCurrency: "EUR",
-            amount: 0,
+            amount: "",
+            change: "",
             rate: null,
-            invert: null,
+            invertRate: null,
             currencies: ["EUR", "GBP", "USD"],
             pockets: [
                 {
                     currency: "EUR",
                     symbol: "€",
-                    amount: 0
+                    balance: 0
                 },
                 {
                     currency: "GBP",
                     symbol: "£",
-                    amount: 1000
+                    balance: 1000
                 },
                 {
                     currency: "USD",
                     symbol: "$",
-                    amount: 0
+                    balance: 0
                 }
             ]
         };
     }
     componentDidMount() {
         this.getRate()
-        this.interval = setInterval(() =>  this.getRate(), 10000);
+        this.interval = setInterval(() => this.getRate(), 10000);
     };
     componentWillUnmount() {
         clearInterval(this.interval);
     };
     getRate = () => {
-        //TODO: add/subtract amounts from pockets
         //TODO: change pockets ?
         //TODO: add tests
         //TODO: limit input to two decial points
-        //TODO: make target input changable too
         axios
             .get(
                 //cors enabled: https://cors-anywhere.herokuapp.com/
@@ -54,86 +52,106 @@ class Converter extends React.Component {
             )
             .then(response => {
                 const rate = response.data.rates[this.state.targetCurrency];
-                const invert = 1 / rate 
+                const invertRate = 1 / rate
                 this.setState({ rate: rate.toFixed(2) });
-                this.setState({ invert: invert.toFixed(2) });
+                this.setState({ invertRate: invertRate.toFixed(2) });
             })
             .catch(error => {
                 console.log("Error Occured when Fetching Rates", error.message);
             });
     };
     convertCurrency = event => {
-        this.setState({ amount: event.target.value });
-        const change = event.target.value * this.state.rate;
-        this.setState({ change: change.toFixed(2) });
+        const value = event.target.value
+        if (event.target.name === "source") {
+            const change = value * this.state.rate;
+            this.setState({ amount: value });
+            this.setState({ change: change === 0 ? '' : change.toFixed(2) });
+        } else {
+            const amount = value * this.state.invertRate;
+            this.setState({ amount: amount.toFixed(2) });
+            this.setState({ change: value });
+        }
+
     };
     clearInputs = () => {
-        this.setState({ change: 0 })
-        this.setState({ amount: 0 })
-    };  
-    updatePocket = (source,target) => {
-        if(this.state.amount > source.amount) return
-        const pockets = this.state.pockets
-        pockets.forEach( _ => {
-            if(_.currency === target.currency) _.amount = (_.amount + parseFloat(this.state.change)).toFixed(2)
-            if(_.currency === source.currency) _.amount = (_.amount - parseFloat(this.state.amount)).toFixed(2)
-        }) 
-        this.setState({ pockets: pockets })
+        this.setState({ change: '' })
+        this.setState({ amount: '' })
+    };
+    updatePocket = (source, target) => {
+        if (this.state.amount > source.amount) return
+        const newPockets = this.state.pockets
+        newPockets.forEach(_ => {
+            if (_.currency === target.currency) _.balance = (parseFloat(_.balance) + parseFloat(this.state.change)).toFixed(2)
+            if (_.currency === source.currency) _.balance = (parseFloat(_.balance) - parseFloat(this.state.amount)).toFixed(2)
+        })
+        this.setState({ pockets: newPockets })
         this.clearInputs()
     };
     render() {
         const source = this.state.pockets.find(_ => _.currency === this.state.sourceCurrency);
         const target = this.state.pockets.find(_ => _.currency === this.state.targetCurrency);
+        const isBalanceSufficient = source.balance >= this.state.amount
         return (
             <div className="converter">
                 <h1>
-                    Convert Currencies
+                    Exchange Credit
                 </h1>
                 <div className="controls">
-                    <button onClick={_ => this.clearInputs()} >Cancel</button>    
-                    <span>
-                     {source.symbol} 1 = {target.symbol} {this.state.rate} 
-                    </span>
-                    <button onClick={_ =>this.updatePocket(source,target)}>Exchange</button>  
+                    <button onClick={_ => this.clearInputs()} >Cancel</button>
+                    {this.state.rate
+                        ? <span> {source.symbol} 1 = {target.symbol} {this.state.rate}</span>
+                        : null}
+                    <button className={isBalanceSufficient ? null : 'disabled'} disabled={!isBalanceSufficient} onClick={_ => this.updatePocket(source, target)} >Exchange</button>
                 </div>
                 <div className="source">
                     <div className="pocket">
                         <label className="currency">
                             {source.currency}
                         </label>
-                        <span className="amount">
-                            You have {source.symbol} {source.amount}
+                        <span className="balance">
+                            You have {source.symbol} {source.balance}
                         </span>
                     </div>
-                    <input
-                        name="amount"
-                        type="number"
-                        required
-                        pattern="^\d*(\.\d{0,2})?$" 
-                        value={this.state.amount}
-                        onChange={event => this.convertCurrency(event)}
-                    />
+                    <div className="exchange">
+                        <span className="sign">-</span>
+                        <input
+                            name="source"
+                            type="number"
+                            placeholder=""
+                            autoFocus
+                            min="0"
+                            value={this.state.amount}
+                            onChange={event => this.convertCurrency(event)}
+                        />
+                    </div>
                 </div>
-                {this.state.amount > source.amount ? <h5>There is not enough credit available for this transaction.</h5>  : ''} 
+                {isBalanceSufficient
+                    ? null
+                    : <span className="error">Not enough credit available.</span>}
                 <div className="target">
                     <div className="pocket">
                         <label className="currency">
                             {target.currency}
                         </label>
-                        <span className="amount">
-                            You have {target.symbol} {target.amount}
+                        <span className="balance">
+                            You have {target.symbol} {target.balance}
                         </span>
                     </div>
-                    <div className="exchange">
-                        <input
-                            name="amount"
-                            type="number"
-                            readOnly
-                            value={this.state.change}
-                        />
-                        <span className="note">
-                            {target.symbol} 1 = {source.symbol} {this.state.invert} 
-                        </span>
+                    <div className="pocket">
+                        <div className="exchange">
+                            <span className="sign">+</span>
+                            <input
+                                name="target"
+                                type="number"
+                                placeholder=""
+                                min="0"
+                                value={this.state.change}
+                                onChange={event => this.convertCurrency(event)}
+                            />
+                        </div>
+                        {this.state.rate
+                            ? <span className="invertRate">{target.symbol} 1 = {source.symbol} {this.state.invertRate}</span>
+                            : null}
                     </div>
                 </div>
             </div>
