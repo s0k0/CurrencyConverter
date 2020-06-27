6717dd8121a26e803b48cb87d34b461c8f4041a7
Sophie Kolbe <soko@Sophies-Air.fritz.box>
2020-06-27 08:58:16 +0200

    add snapshots test

--- src/component/converter.test.js
+++ src/component/converter.test.js
@@ -3,8 +3,7 @@ import { render } from '@testing-library/react';
 import Converter from './converter';
 
 test('renders converter component', () => {
-  const { getByText } = render(<Converter />);
-  const converterHeadline = getByText(/exchange credit/i);
-  expect(converterHeadline).toBeInTheDocument();
+  const container = render(<Converter />);
+  expect(container).toMatchSnapshot()
 });
 
