ea72402c9af3c77822057252c32781233b57e49a
Sophie Kolbe <soko@Sophies-Air.fritz.box>
2020-06-27 19:07:15 +0200

    add test for fetching rate from dummy axios mock

--- src/component/converter.test.js
+++ src/component/converter.test.js
@@ -1,9 +1,34 @@
+
 import React from 'react';
-import { render } from '@testing-library/react';
+import axios from 'axios';
+import { render, waitForElement } from '@testing-library/react';
 import Converter from './converter';
 
-test('renders converter component', () => {
-  const container = render(<Converter />);
-  expect(container).toMatchSnapshot()
-});
+jest.mock('axios')
 
+function mockCall() {
+  axios.get.mockResolvedValueOnce({
+    data: {
+       base: 'GBP',
+       rates: { EUR: 1.1234}
+    }
+  });
+} 
+
+describe('Converter component', () => {
+
+  test('renders converter component', () => {
+    expect(Converter).toMatchSnapshot()
+  });
+
+  test('should fetch rates', async() => {
+    mockCall();
+    const url = 'https://cors-anywhere.herokuapp.com/http://api.openrates.io/latest?base=GBP&symbols=EUR'
+    const { getByText } = render(<Converter />);
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(url);
+    //check whether exchange rate from mock response is received and rendered into component
+    const rate = await waitForElement(() => getByText(/1.12/i))
+    expect(rate).toBeInTheDocument();
+  });
+});
